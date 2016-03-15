202b71d48c9de7a7e2745522444b2368c30c1358
黄胜庆 <huangshengqing@qtone.cn>
2016-03-15 17:47:24 +0800

    添加操作db、zk的demo

--- webDemo/web-war/src/main/java/com/test360/controller/TestController.java
+++ webDemo/web-war/src/main/java/com/test360/controller/TestController.java
@@ -1,11 +1,12 @@
 package com.test360.controller;
 
-import cn.thinkjoy.cloudstack.client.zookeeper.ZKClient;
 import cn.thinkjoy.cloudstack.dynconfig.DynConfigClient;
 import cn.thinkjoy.cloudstack.dynconfig.DynConfigClientFactory;
-import cn.thinkjoy.common.protocol.RequestT;
 import com.test360.business.model.Criteria;
+import com.test360.business.model.User;
 import com.test360.business.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,8 +28,9 @@ import java.util.Map;
 @RequestMapping(value = "/test")
 public class TestController {
 
-//    @Autowired
-//    UserService userService;
+    Logger logger= LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    UserService userService;
 
     @ResponseBody
     @RequestMapping(value = "/test01")
@@ -36,10 +39,21 @@ public class TestController {
         DynConfigClient dynConfigClient = DynConfigClientFactory.getClient();
 
         try {
-//            Criteria criteria=  new Criteria();
+
+            //insertdemo
+            User user=new User();
+            user.setName("李四");
+            user.setPassword("123456");
+            user.setBrithday(new Date());
+//            userService.insertSelective(user);
+
+            //selectdemo
+            Criteria criteria=  new Criteria();
 //            criteria.put("name_greater","%李四%");
-//            userService.selectByExample(criteria);
-            System.out.println("  "+dynConfigClient.getConfig("common", "redisip"));
+
+            map.put("user",userService.selectByExample(criteria));
+            //通过zookeeper获取配置文件
+            logger.info(dynConfigClient.getConfig("common", "redisip"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -50,10 +64,10 @@ public class TestController {
 
     @ResponseBody
     @RequestMapping(value = "/login")
-    public Object login(HttpServletRequest request, HttpServletResponse response, @RequestBody RequestT<Map<String, String>> parames) {
+    public Object login(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String, Object> parames) {
         HashMap<String, Object> map = new HashMap<String, Object>();
 
-        System.out.println(parames.toString());
+        logger.info(parames.toString());
         map.put("success",true);
         map.put("msg","登录成功");
 
